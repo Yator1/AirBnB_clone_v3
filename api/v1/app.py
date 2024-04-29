19218a4ab062dfdeec15a29c25a59b59b8551c43
Yator1 <titusyatz17@gmail.com>
2024-04-29 22:34:47 +0300

    Error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Creating a flask api
 """
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.vi.views import app_views
 
@@ -12,7 +12,23 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
+@app.close_app
+def close_engine(exception):
+    """
+    """
+    storage.close()
+
+
+@app.errorhandle(404)
+def not_found(error):
+    """
+    """
+    response = {"error": "Not found"}
+    return jsonify(response), 404
+
+
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(debug=True, host=HOST, port=PORT, threaded=True)
