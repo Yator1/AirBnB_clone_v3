ba9b78d14231975cedb0246b99f5b4fd0fc2ac68
Yator1 <titusyatz17@gmail.com>
2024-04-29 20:43:07 +0300

    Status of API

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Creating Flask app
+"""
+
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status')
+def api_status():
+    """
+    """
+    resp = {'status': "OK"}
+    return jsonify(resp)
